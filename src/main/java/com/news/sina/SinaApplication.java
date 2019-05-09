6600ef4a53abde25e1fd2361aed8182f8b5182e1
也许丶 <myyearn545@gmail.com>
2019-05-09 12:54:12 +0800

    Initial commit

---
+++ src/main/java/com/news/sina/SinaApplication.java
@@ -0,0 +1,13 @@
+package com.news.sina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinaApplication.class, args);
+	}
+
+}
